3960617c95062b116e09ef55d2e168ebc5191975
janna-n <jrose.n008@gmail.com>
2026-07-08 09:54:28 -0600

    Update visitor count

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBM5-cQema1PzjpTPtCxIdmvTrWH2p1nc8",
-  authDomain: "vinaysomawat-portfolio.firebaseapp.com",
-  projectId: "vinaysomawat-portfolio",
-  storageBucket: "vinaysomawat-portfolio.firebasestorage.app",
-  messagingSenderId: "996992279996",
-  appId: "1:996992279996:web:f296555eef216d501337b9",
-  measurementId: "G-8PVX0MLGSK",
+  apiKey: "AIzaSyAhHyWq1lCuZaSVGGFxY328eBTkacLNHZI",
+  authDomain: "jannanystrom-portfolio.firebaseapp.com",
+  projectId: "jannanystrom-portfolio",
+  storageBucket: "jannanystrom-portfolio.firebasestorage.app",
+  messagingSenderId: "923159102601",
+  appId: "1:923159102601:web:b4de203e86918942bb3258",
+  measurementId: "G-RM0NEG9S6P"
 };
